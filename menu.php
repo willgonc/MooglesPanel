af5d6fde22c2de2f5939b9e6199156ea5856af22
markusslima <devnull@localhost>
2011-08-05 14:34:36 -0300

    adicionando arquivos do inicio do projeto

---
+++ menu.php
@@ -0,0 +1,10 @@
+<?php 
+    print '
+        <div class="menu">
+            <ul>
+                <li><a href="resumo.php">Resumo</a></li>
+                <li><a href="usuarios.php">Usu&aacute;rios</a></li>
+            </ul>
+        </div>
+    ';
+?>
---
+++ usuarios.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    </head>
+    <body>
+        <?php require_once "menu.php"; ?>
+    </body>
+</html>
+
