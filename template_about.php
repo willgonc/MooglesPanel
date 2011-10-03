087b8d3efb9a6e2d78eda17f1e5dcfc1fd2ccbf8
markusslima <devnull@localhost>
2011-10-03 11:47:53 -0300

    Removendo arquivos de template

--- template_about.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "lib_ui.php"; 
-printCabecalho('Sobre o Painel de controle');
-printMsg();
-?>
-<div class="tw-ui-content">
-    <div class="tw-ui-content-mod">
-    </div>
-</div>
