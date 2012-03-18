968c72543ea8700a8badf0fbfd6f990eb7c11adb
Markus Lima <markusslima@gmail.com>
2012-03-18 14:32:45 -0300

    Formatando tela de login

--- js/autenticacao.js
+++ js/autenticacao.js
@@ -1,17 +1,24 @@
-function verificaAutenticacao(param){
-	$.getJSON("../../VerificaAutenticacao.php", function (data){
-		if (data.resposta) {
-			if (param)
-				window.location = "../../";
-			else
-				init();
+function verificaAutenticacao(){
+	$.ajax({
+	    type: 'GET',
+		url: "../../VerificaAutenticacao.php",
+		dataType: 'json',
+		success: function(data) {
+			var arrURL = window.location.pathname.split('/');
+			var mod = arrURL[arrURL.length - 2];
+			if (data.resposta) {
+				if (mod == 'login')
+					window.location = "../../";
 
-		} else {
-			if (param)
-				init();
-			else
-				window.location = "../../modulos/login/";
+			} else {
+				if (mod != 'login')
+					window.location = "../../modulos/login/";
 
-		}
+			}
+		},
+		data: {},
+		async: false
 	});
 }
+
+verificaAutenticacao();
--- modulos/login/js/lib.js
+++ modulos/login/js/lib.js
@@ -1,24 +1,27 @@
 function init(){
+	$('#load').hide();
 	$('#email').focus();
 
 	$('#email, #senha').keypress(function (){
-		if (event.keyCode == 13)
+		if (event.keyCode == 13){
 			autenticaUsuario($('#email').val(), $('#senha').val());
+			$('#load').show();
+		}
 	});
 	$('#botaoEntrar').click(function (){
+		$('#load').show();
 		autenticaUsuario($('#email').val(), $('#senha').val());
 	});
 }
-var teste;
-
 
 function autenticaUsuario(email, senha){
 	$.getJSON("AutenticaUsuario.php",{ "email": email, "senha": senha }, function (data){
-		teste = data;
+		setTimeout("$('#load').hide()",2000);
+		
 		if (data.resposta)
 			window.location = "../../index.php";
 		else
-			alert('Usuario ou senha incorretos!');
+			$('#msg').html('Usuario ou senha incorretos!');
 		
 	});
 }
