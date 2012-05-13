04cba5b4c6b2df770737fbed58c914bfc9873e3b
Markus Lima <markusslima@gmail.com>
2012-05-12 23:43:44 -0300

    Removendo arquivos não usados

--- js/dropdown.js
+++
@@ -1,7 +0,0 @@
-function dropdown(){
-    $('.item-menu').mouseover(function (){
-        $(this).children('.submenu').show();
-    }).mouseout(function (){
-        $(this).children('.submenu').hide();
-    });
-}
